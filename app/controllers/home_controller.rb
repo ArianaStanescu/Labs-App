2d7cf97e9423fe4055870f79b8193b3d77516244
Ariana Stanescu <stanescu.ariana02@gmail.com>
2023-05-18 17:34:40 +0300

    sort order added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,7 @@ class HomeController < ApplicationController
 
   def all_products
     @products = Product.all
+    @sort_order = params[:sort_order] || 'asc' # Ordinea de sortare implicită este ascendentă
+    @products = Product.order(price: @sort_order)
   end
 end
