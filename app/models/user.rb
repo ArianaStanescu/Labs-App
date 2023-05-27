57b22bda7c84e483384029cde0f6489444d1a735
Ariana Stanescu <stanescu.ariana02@gmail.com>
2023-05-27 12:12:47 +0300

    lab27_05

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :credit_cards
+  has_many :orders
   accepts_nested_attributes_for :credit_cards
   validates :phone, presence: true, length: { is: 10 }
 end
