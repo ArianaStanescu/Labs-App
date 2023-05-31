57b73e830e8647191c411c8aad38c1894911b8ff
Ariana Stanescu <stanescu.ariana02@gmail.com>
2023-05-31 13:17:38 +0300

    added products pagy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except: [:index, :all_products]
+  include Pagy::Backend
   # def new
   # end
 
@@ -8,8 +9,8 @@ class HomeController < ApplicationController
 
   def all_products
     @sort_order = params[:sort_order] || 'asc'
-    @products = Product.includes(image_attachment: :blob).order(price: @sort_order)
-
+    # @products = Product.includes(image_attachment: :blob).order(price: @sort_order)
+    @pagy, @products = pagy(Product.includes(image_attachment: :blob).order(price: @sort_order))
     # @products = Product.includes(image_attachment: :blob).all
 
     # @products = Product.all
--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,4 +1,6 @@
 class CreditCard < ApplicationRecord
   belongs_to :user
   # belongs_to :user, :optional => true
+  validates :number, presence: true, length: { is: 16 }
+  # validates :cvv, presence: true, length: { is: 3 }
 end
