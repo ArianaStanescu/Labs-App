9abc906172304fb219a6bd712a873ed4bfc5adfb
Ariana Stanescu <stanescu.ariana02@gmail.com>
2023-06-01 19:24:09 +0300

    alerts are visible

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -9,19 +9,19 @@ class CreditCardsController < ApplicationController
   end
 
   # GET /credit_cards/1 or /credit_cards/1.json
-  def show
-    @credit_card = CreditCard.find(params[:id])
-    if @credit_card.user_id != current_user.id
-      redirect_to root_path, alert: "You are not authorized to edit this credit card."
-    end
-  end
-
   # def show
   #   @credit_card = CreditCard.find(params[:id])
   #   if @credit_card.user_id != current_user.id
-  #     flash[:alert] = "You are not authorized to view this credit card."
+  #     redirect_to root_path, alert: "You are not authorized to edit this credit card."
   #   end
   # end
+
+  def show
+    @credit_card = CreditCard.find(params[:id])
+    if @credit_card.user_id != current_user.id
+      redirect_to root_path, alert: 'You are not authorized to view this credit card.'
+    end
+  end
 
   # GET /credit_cards/new
   def new
